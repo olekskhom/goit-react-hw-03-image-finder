c9d0422a40d1a74edff74ebbc6cf92a0f5a38462
olekskhom <oleks.khom@gmail.com>
2023-10-03 10:34:49 +0300

    fix_all

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,23 +1,3 @@
-// import PropTypes from 'prop-types';
-// import css from './Button.module.css';
-
-// export function Button({ onClick, hasImages }) {
-//   return (
-//     <>
-//       {hasImages && (
-//         <button className={css.button} type="button" onClick={onClick}>
-//           Load more
-//         </button>
-//       )}
-//     </>
-//   );
-// }
-
-// Button.propTypes = {
-//   onClick: PropTypes.func.isRequired,
-// hasImages: PropTypes.bool.isRequired,
-// };
-
 import PropTypes from 'prop-types';
 import css from './Button.module.css';
 
@@ -35,7 +15,7 @@ export function Button({ onClick, hasImages }) {
 
 Button.propTypes = {
   onClick: PropTypes.func.isRequired,
-  hasImages: PropTypes.bool.isRequired, // Додайте пропс для визначення наявності зображень на бекенді
+  hasImages: PropTypes.bool.isRequired, 
 };
 
 
